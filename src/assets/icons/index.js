66b88a72e5296bf82649ec579afc170d176aedd0
sanya-shch <shchipkov.sanya@ukr.net>
2022-07-18 23:59:52 +0300

    added icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,121 @@
+import cat1 from "./cats/cat.png";
+import cat2 from "./cats/cat (1).png";
+import cat3 from "./cats/cat (2).png";
+import cat4 from "./cats/cat (3).png";
+import cat5 from "./cats/cat (4).png";
+import cat6 from "./cats/cat (5).png";
+import cat7 from "./cats/cat (6).png";
+import cat8 from "./cats/cat (16).png";
+import cat9 from "./cats/cat (7).png";
+import cat10 from "./cats/cat (8).png";
+import cat11 from "./cats/cat (9).png";
+import cat12 from "./cats/cat (10).png";
+import cat13 from "./cats/cat (11).png";
+import cat14 from "./cats/cat (12).png";
+import cat15 from "./cats/cat (13).png";
+import cat16 from "./cats/cat (14).png";
+import cat17 from "./cats/cat (15).png";
+
+import animals1 from "./animals/bear.png";
+import animals2 from "./animals/bull.png";
+import animals3 from "./animals/deer.png";
+import animals4 from "./animals/elephant.png";
+import animals5 from "./animals/giraffe.png";
+import animals6 from "./animals/gorilla.png";
+import animals7 from "./animals/hedgehog.png";
+import animals8 from "./animals/hippopotamus.png";
+import animals9 from "./animals/lion.png";
+import animals10 from "./animals/monkey.png";
+import animals11 from "./animals/owl.png";
+import animals12 from "./animals/panda-bear.png";
+import animals13 from "./animals/penguin.png";
+import animals14 from "./animals/rabbit.png";
+import animals15 from "./animals/rhinoceros.png";
+import animals16 from "./animals/tiger.png";
+import animals17 from "./animals/turtle.png";
+import animals18 from "./animals/wolf.png";
+
+import origami1 from "./origami/rabbit.png";
+import origami2 from "./origami/penguin.png";
+import origami3 from "./origami/giraffe.png";
+import origami4 from "./origami/elephant.png";
+import origami5 from "./origami/bear.png";
+import origami6 from "./origami/bird (1).png";
+import origami7 from "./origami/butterfly.png";
+import origami8 from "./origami/crane.png";
+import origami9 from "./origami/dog.png";
+import origami10 from "./origami/dove.png";
+import origami11 from "./origami/fish.png";
+import origami12 from "./origami/fox.png";
+import origami13 from "./origami/frog.png";
+import origami14 from "./origami/pig.png";
+import origami15 from "./origami/rooster.png";
+import origami16 from "./origami/seal.png";
+import origami17 from "./origami/star.png";
+
+const cat = {
+  cat1,
+  cat2,
+  cat3,
+  cat4,
+  cat5,
+  cat6,
+  cat7,
+  cat8,
+  cat9,
+  cat10,
+  cat11,
+  cat12,
+  cat13,
+  cat14,
+  cat15,
+  cat16,
+  cat17,
+};
+
+const animals = {
+  animals1,
+  animals2,
+  animals3,
+  animals4,
+  animals5,
+  animals6,
+  animals7,
+  animals8,
+  animals9,
+  animals10,
+  animals11,
+  animals12,
+  animals13,
+  animals14,
+  animals15,
+  animals16,
+  animals17,
+  animals18,
+};
+
+const origami = {
+  origami1,
+  origami2,
+  origami3,
+  origami4,
+  origami5,
+  origami6,
+  origami7,
+  origami8,
+  origami9,
+  origami10,
+  origami11,
+  origami12,
+  origami13,
+  origami14,
+  origami15,
+  origami16,
+  origami17,
+};
+
+export {
+  cat,
+  animals,
+  origami,
+}
