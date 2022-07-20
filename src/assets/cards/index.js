09757ef76120ada2e7cd9bd9b47ddb90a109e160
sanya-shch <shchipkov.sanya@ukr.net>
2022-07-20 23:14:37 +0300

    added game cards

---
+++ src/assets/cards/index.js
@@ -0,0 +1,119 @@
+// ImplodingKittens
+import alterTheFuture_1 from './ImplodingKittens/exploding-kittens-alterfuture1.png';
+import alterTheFuture_2 from './ImplodingKittens/exploding-kittens-alterfuture2.png';
+import alterTheFuture_3 from './ImplodingKittens/exploding-kittens-alterfuture3.png';
+import alterTheFuture_4 from './ImplodingKittens/exploding-kittens-ALTERFUTURE4.png';
+import feral_1 from './ImplodingKittens/exploding-kittens-feral.png';
+import drawFromTheBottom_1 from './ImplodingKittens/exploding-kittens-bottom1.png';
+import drawFromTheBottom_2 from './ImplodingKittens/exploding-kittens-bottom2.png';
+import drawFromTheBottom_3 from './ImplodingKittens/exploding-kittens-bottom3.png';
+import drawFromTheBottom_4 from './ImplodingKittens/exploding-kittens-bottom4.png';
+import impendingImplodingKitten_1 from './ImplodingKittens/exploding-kittens-IIK.png';
+import reverse_1 from './ImplodingKittens/exploding-kittens-revers1.png';
+import reverse_2 from './ImplodingKittens/exploding-kittens-reverse2.png';
+import reverse_3 from './ImplodingKittens/exploding-kittens-reverse3.png';
+import reverse_4 from './ImplodingKittens/exploding-kittens-reverse4.png';
+import targetedAttack_1 from './ImplodingKittens/exploding-kittens-tattack-3.png';
+import targetedAttack_2 from './ImplodingKittens/exploding-kittens-tattack-5.png';
+import targetedAttack_3 from './ImplodingKittens/exploding-kittens-tattack-6.png';
+
+// ExplodingKittensOriginal
+import explodingKitten_1 from './ExplodingKittens_ORIGINAL/exploding-kittens-1.png';
+import explodingKitten_2 from './ExplodingKittens_ORIGINAL/exploding-kittens-2.png';
+import explodingKitten_3 from './ExplodingKittens_ORIGINAL/exploding-kittens-3.png';
+import explodingKitten_4 from './ExplodingKittens_ORIGINAL/exploding-kittens-8.png';
+import attack_1 from './ExplodingKittens_ORIGINAL/exploding-kittens-attack-1.png';
+import attack_2 from './ExplodingKittens_ORIGINAL/exploding-kittens-attack-2.png';
+import attack_3 from './ExplodingKittens_ORIGINAL/exploding-kittens-attack-3.png';
+import attack_4 from './ExplodingKittens_ORIGINAL/exploding-kittens-attack-10.png';
+import defuse_1 from './ExplodingKittens_ORIGINAL/exploding-kittens-defuse-1.png';
+import defuse_2 from './ExplodingKittens_ORIGINAL/exploding-kittens-defuse-2.png';
+import defuse_3 from './ExplodingKittens_ORIGINAL/exploding-kittens-defuse-4.png';
+import defuse_4 from './ExplodingKittens_ORIGINAL/exploding-kittens-defuse-7.png';
+import defuse_5 from './ExplodingKittens_ORIGINAL/exploding-kittens-defuse-9.png';
+import defuse_6 from './ExplodingKittens_ORIGINAL/exploding-kittens-defuse-11.png';
+import favor_1 from './ExplodingKittens_ORIGINAL/exploding-kittens-favor-1.png';
+import favor_2 from './ExplodingKittens_ORIGINAL/exploding-kittens-favor-2.png';
+import favor_3 from './ExplodingKittens_ORIGINAL/exploding-kittens-favor-6.png';
+// import favor_4 from './ExplodingKittens_ORIGINAL';
+import nope_1 from './ExplodingKittens_ORIGINAL/exploding-kittens-nope-1.png';
+import nope_2 from './ExplodingKittens_ORIGINAL/exploding-kittens-nope-7.png';
+import nope_3 from './ExplodingKittens_ORIGINAL/exploding-kittens-nope-8.png';
+import nope_4 from './ExplodingKittens_ORIGINAL/exploding-kittens-nope-9.png';
+import nope_5 from './ExplodingKittens_ORIGINAL/exploding-kittens-nope-11.png';
+import seeFuture_1 from './ExplodingKittens_ORIGINAL/exploding-kittens-seefuture1.png';
+import seeFuture_2 from './ExplodingKittens_ORIGINAL/exploding-kittens-SEEFUTURE6.png';
+import seeFuture_3 from './ExplodingKittens_ORIGINAL/exploding-kittens-seefuture7.png';
+import seeFuture_4 from './ExplodingKittens_ORIGINAL/exploding-kittens-SEEFUTURE9.png';
+// import seeFuture_5 from './ExplodingKittens_ORIGINAL';
+import shuffle_1 from './ExplodingKittens_ORIGINAL/exploding-kittens-shuffle-2.png';
+import shuffle_2 from './ExplodingKittens_ORIGINAL/exploding-kittens-shuffle-3.png';
+import shuffle_3 from './ExplodingKittens_ORIGINAL/exploding-kittens-shuffle-5.png';
+// import shuffle_4 from './ExplodingKittens_ORIGINAL';
+import skip_1 from './ExplodingKittens_ORIGINAL/exploding-kittens-skip3.png';
+import skip_2 from './ExplodingKittens_ORIGINAL/exploding-kittens-SKIP4.png';
+import skip_3 from './ExplodingKittens_ORIGINAL/exploding-kittens-skip5.png';
+// import skip_4 from './ExplodingKittens_ORIGINAL';
+import beardCat_1 from './ExplodingKittens_ORIGINAL/exploding-kittens-standard-3.png';
+import cattermelon_1 from './ExplodingKittens_ORIGINAL/exploding-kittens-standard-4.png';
+import hairyPotatoCat_1 from './ExplodingKittens_ORIGINAL/exploding-kittens-standard-5.png';
+import tacocat_1 from './ExplodingKittens_ORIGINAL/exploding-kittens-standard-6.png';
+import rainbowRalphingCat_1 from './ExplodingKittens_ORIGINAL/exploding-kittens-standard-7.png';
+
+export {
+  alterTheFuture_1,
+  alterTheFuture_2,
+  alterTheFuture_3,
+  alterTheFuture_4,
+  feral_1,
+  drawFromTheBottom_1,
+  drawFromTheBottom_2,
+  drawFromTheBottom_3,
+  drawFromTheBottom_4,
+  impendingImplodingKitten_1,
+  reverse_1,
+  reverse_2,
+  reverse_3,
+  reverse_4,
+  targetedAttack_1,
+  targetedAttack_2,
+  targetedAttack_3,
+
+  explodingKitten_1,
+  explodingKitten_2,
+  explodingKitten_3,
+  explodingKitten_4,
+  attack_1,
+  attack_2,
+  attack_3,
+  attack_4,
+  defuse_1,
+  defuse_2,
+  defuse_3,
+  defuse_4,
+  defuse_5,
+  defuse_6,
+  favor_1,
+  favor_2,
+  favor_3,
+  nope_1,
+  nope_2,
+  nope_3,
+  nope_4,
+  nope_5,
+  seeFuture_1,
+  seeFuture_2,
+  seeFuture_3,
+  seeFuture_4,
+  shuffle_1,
+  shuffle_2,
+  shuffle_3,
+  skip_1,
+  skip_2,
+  skip_3,
+  beardCat_1,
+  cattermelon_1,
+  hairyPotatoCat_1,
+  tacocat_1,
+  rainbowRalphingCat_1,
+}
---
+++ src/constants/cards.js
@@ -0,0 +1,375 @@
+import * as cardsImages from '../assets/cards';
+
+export const ImplodingKittens = {
+  alterTheFuture_1: {
+    img: cardsImages.alterTheFuture_1,
+    title: "Alter The Future",
+  },
+  alterTheFuture_2: {
+    img: cardsImages.alterTheFuture_2,
+    title: "Alter The Future",
+  },
+  alterTheFuture_3: {
+    img: cardsImages.alterTheFuture_3,
+    title: "Alter The Future",
+  },
+  alterTheFuture_4: {
+    img: cardsImages.alterTheFuture_4,
+    title: "Alter The Future",
+  },
+  drawFromTheBottom_1: {
+    img: cardsImages.drawFromTheBottom_1,
+    title: "Draw From The Bottom",
+  },
+  drawFromTheBottom_2: {
+    img: cardsImages.drawFromTheBottom_2,
+    title: "Draw From The Bottom",
+  },
+  drawFromTheBottom_3: {
+    img: cardsImages.drawFromTheBottom_3,
+    title: "Draw From The Bottom",
+  },
+  drawFromTheBottom_4: {
+    img: cardsImages.drawFromTheBottom_4,
+    title: "Draw From The Bottom",
+  },
+  feral_1: {
+    img: cardsImages.feral_1,
+    title: "Feral Cat",
+  },
+  feral_2: {
+    img: cardsImages.feral_1,
+    title: "Feral Cat",
+  },
+  feral_3: {
+    img: cardsImages.feral_1,
+    title: "Feral Cat",
+  },
+  feral_4: {
+    img: cardsImages.feral_1,
+    title: "Feral Cat",
+  },
+  impendingImplodingKitten_1: {
+    img: cardsImages.impendingImplodingKitten_1,
+    title: "Impending Imploding Kitten",
+  },
+  reverse_1: {
+    img: cardsImages.reverse_1,
+    title: "Reverse",
+  },
+  reverse_2: {
+    img: cardsImages.reverse_2,
+    title: "Reverse",
+  },
+  reverse_3: {
+    img: cardsImages.reverse_3,
+    title: "Reverse",
+  },
+  reverse_4: {
+    img: cardsImages.reverse_4,
+    title: "Reverse",
+  },
+  targetedAttack_1: {
+    img: cardsImages.targetedAttack_1,
+    title: "Targeted Attack",
+  },
+  targetedAttack_2: {
+    img: cardsImages.targetedAttack_2,
+    title: "Targeted Attack",
+  },
+  targetedAttack_3: {
+    img: cardsImages.targetedAttack_3,
+    title: "Targeted Attack",
+  },
+};
+
+export const ExplodingKittensOriginal = {
+  explodingKitten_1: {
+    img: cardsImages.explodingKitten_1,
+    title: "Exploding Kitten",
+    type: "explodingKitten"
+  },
+  explodingKitten_2: {
+    img: cardsImages.explodingKitten_2,
+    title: "Exploding Kitten",
+    type: "explodingKitten"
+  },
+  explodingKitten_3: {
+    img: cardsImages.explodingKitten_3,
+    title: "Exploding Kitten",
+    type: "explodingKitten"
+  },
+  explodingKitten_4: {
+    img: cardsImages.explodingKitten_4,
+    title: "Exploding Kitten",
+    type: "explodingKitten"
+  },
+  attack_1: {
+    img: cardsImages.attack_1,
+    title: "Attack",
+    type: "attack"
+  },
+  attack_2: {
+    img: cardsImages.attack_2,
+    title: "Attack",
+    type: "attack"
+  },
+  attack_3: {
+    img: cardsImages.attack_3,
+    title: "Attack",
+    type: "attack"
+  },
+  attack_4: {
+    img: cardsImages.attack_4,
+    title: "Attack",
+    type: "attack"
+  },
+  defuse_1: {
+    img: cardsImages.defuse_1,
+    title: "Defuse",
+    type: "defuse"
+  },
+  defuse_2: {
+    img: cardsImages.defuse_2,
+    title: "Defuse",
+    type: "defuse"
+  },
+  defuse_3: {
+    img: cardsImages.defuse_3,
+    title: "Defuse",
+    type: "defuse"
+  },
+  defuse_4: {
+    img: cardsImages.defuse_4,
+    title: "Defuse",
+    type: "defuse"
+  },
+  defuse_5: {
+    img: cardsImages.defuse_5,
+    title: "Defuse",
+    type: "defuse"
+  },
+  defuse_6: {
+    img: cardsImages.defuse_6,
+    title: "Defuse",
+    type: "defuse"
+  },
+  favor_1: {
+    img: cardsImages.favor_1,
+    title: "Favor",
+    type: "favor"
+  },
+  favor_2: {
+    img: cardsImages.favor_2,
+    title: "Favor",
+    type: "favor"
+  },
+  favor_3: {
+    img: cardsImages.favor_3,
+    title: "Favor",
+    type: "favor"
+  },
+  favor_4: {
+    img: cardsImages.zzzzz,
+    title: "Favor",
+    type: "favor"
+  },
+  nope_1: {
+    img: cardsImages.nope_1,
+    title: "Nope",
+    type: "nope"
+  },
+  nope_2: {
+    img: cardsImages.nope_2,
+    title: "Nope",
+    type: "nope"
+  },
+  nope_3: {
+    img: cardsImages.nope_3,
+    title: "Nope",
+    type: "nope"
+  },
+  nope_4: {
+    img: cardsImages.nope_4,
+    title: "Nope",
+    type: "nope"
+  },
+  nope_5: {
+    img: cardsImages.nope_5,
+    title: "Nope",
+    type: "nope"
+  },
+  seeFuture_1: {
+    img: cardsImages.seeFuture_1,
+    title: "See The Future",
+    type: "seeFuture"
+  },
+  seeFuture_2: {
+    img: cardsImages.seeFuture_2,
+    title: "See The Future",
+    type: "seeFuture"
+  },
+  seeFuture_3: {
+    img: cardsImages.seeFuture_3,
+    title: "See The Future",
+    type: "seeFuture"
+  },
+  seeFuture_4: {
+    img: cardsImages.seeFuture_4,
+    title: "See The Future",
+    type: "seeFuture"
+  },
+  seeFuture_5: {
+    img: cardsImages.zzzzz,
+    title: "See The Future",
+    type: "seeFuture"
+  },
+  shuffle_1: {
+    img: cardsImages.shuffle_1,
+    title: "Shuffle",
+    type: "shuffle"
+  },
+  shuffle_2: {
+    img: cardsImages.shuffle_2,
+    title: "Shuffle",
+    type: "shuffle"
+  },
+  shuffle_3: {
+    img: cardsImages.shuffle_3,
+    title: "Shuffle",
+    type: "shuffle"
+  },
+  shuffle_4: {
+    img: cardsImages.zzzzz,
+    title: "Shuffle",
+    type: "shuffle"
+  },
+  skip_1: {
+    img: cardsImages.skip_1,
+    title: "Skip",
+    type: "skip"
+  },
+  skip_2: {
+    img: cardsImages.skip_2,
+    title: "Skip",
+    type: "skip"
+  },
+  skip_3: {
+    img: cardsImages.skip_3,
+    title: "Skip",
+    type: "skip"
+  },
+  skip_4: {
+    img: cardsImages.zzzzz,
+    title: "Skip",
+    type: "skip"
+  },
+  beardCat_1: {
+    img: cardsImages.beardCat_1,
+    title: "Beard Cat",
+    type: "standardCart"
+  },
+  beardCat_2: {
+    img: cardsImages.beardCat_1,
+    title: "Beard Cat",
+    type: "standardCart"
+  },
+  beardCat_3: {
+    img: cardsImages.beardCat_1,
+    title: "Beard Cat",
+    type: "standardCart"
+  },
+  beardCat_4: {
+    img: cardsImages.beardCat_1,
+    title: "Beard Cat",
+    type: "standardCart"
+  },
+  cattermelon_1: {
+    img: cardsImages.cattermelon_1,
+    title: "Cattermelon",
+    type: "standardCart"
+  },
+  cattermelon_2: {
+    img: cardsImages.cattermelon_1,
+    title: "Cattermelon",
+    type: "standardCart"
+  },
+  cattermelon_3: {
+    img: cardsImages.cattermelon_1,
+    title: "Cattermelon",
+    type: "standardCart"
+  },
+  cattermelon_4: {
+    img: cardsImages.cattermelon_1,
+    title: "Cattermelon",
+    type: "standardCart"
+  },
+  hairyPotatoCat_1: {
+    img: cardsImages.hairyPotatoCat_1,
+    title: "Hairy Potato Cat",
+    type: "standardCart"
+  },
+  hairyPotatoCat_2: {
+    img: cardsImages.hairyPotatoCat_1,
+    title: "Hairy Potato Cat",
+    type: "standardCart"
+  },
+  hairyPotatoCat_3: {
+    img: cardsImages.hairyPotatoCat_1,
+    title: "Hairy Potato Cat",
+    type: "standardCart"
+  },
+  hairyPotatoCat_4: {
+    img: cardsImages.hairyPotatoCat_1,
+    title: "Hairy Potato Cat",
+    type: "standardCart"
+  },
+  tacocat_1: {
+    img: cardsImages.tacocat_1,
+    title: "Tacocat",
+    type: "standardCart"
+  },
+  tacocat_2: {
+    img: cardsImages.tacocat_1,
+    title: "Tacocat",
+    type: "standardCart"
+  },
+  tacocat_3: {
+    img: cardsImages.tacocat_1,
+    title: "Tacocat",
+    type: "standardCart"
+  },
+  tacocat_4: {
+    img: cardsImages.tacocat_1,
+    title: "Tacocat",
+    type: "standardCart"
+  },
+  rainbowRalphingCat_1: {
+    img: cardsImages.rainbowRalphingCat_1,
+    title: "Rainbow-Ralphing Cat",
+    type: "standardCart"
+  },
+  rainbowRalphingCat_2: {
+    img: cardsImages.rainbowRalphingCat_1,
+    title: "Rainbow-Ralphing Cat",
+    type: "standardCart"
+  },
+  rainbowRalphingCat_3: {
+    img: cardsImages.rainbowRalphingCat_1,
+    title: "Rainbow-Ralphing Cat",
+    type: "standardCart"
+  },
+  rainbowRalphingCat_4: {
+    img: cardsImages.rainbowRalphingCat_1,
+    title: "Rainbow-Ralphing Cat",
+    type: "standardCart"
+  },
+};
+
+const ExplodingKittensNSFW = {
+
+};
+
+const ExplodingKittensParty = {
+
+};
