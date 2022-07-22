a4e813da79e9c408ed3f5da61d05598b8267472d
sanya-shch <shchipkov.sanya@ukr.net>
2022-07-22 21:06:53 +0300

    added toast component

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 // import { HashRouter } from 'react-router-dom';
 
+import ToastProvider from './components/Toast';
 import App from './App';
 
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <ToastProvider>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </ToastProvider>
 );
